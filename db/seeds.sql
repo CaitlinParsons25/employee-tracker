3faa6239d84d5680ba4e5dd70ad3cfa4289984d4
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-07-22 17:51:44 -0500

    added databases and view queries

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (department_name)
+VALUES
+('Legal'),
+('HR'),
+('IT'),
+('Sales'),
+('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Lawyer', '90000.00', 1),
+('Administrator', '50000.00', 2),
+('Engineer', '70000.00', 3),
+('Account Executive', '60000.00', 4),
+('Market Analyst', '80000.00', 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Kaysie', 'Anderson', 3, NULL),
+('Caitlin', 'Parsons', 4, 5),
+('Todd', 'Holley', 2, 1),
+('Temple', 'Kramer', 1, 3),
+('Josie', 'Sparling', 5, NULL);
+
