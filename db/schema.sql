42ae61992a711b6a83fe68a7432671dc8b897273
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-08-11 14:40:17 -0500

    added seed update

--- db/schema.sql
+++ db/schema.sql
@@ -24,7 +24,5 @@ CREATE TABLE employee (
     role_id INTEGER,
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
 );
